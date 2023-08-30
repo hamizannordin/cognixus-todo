b95f33bd8565bde465bd75f729ceda3548375869
hamizannordin <hamizan.nordin@gmail.com>
2023-08-30 16:10:31 +0800

    init spring

---
+++ src/test/java/cognixus/todo/ToDoApplicationTests.java
@@ -0,0 +1,17 @@
+package cognixus.todo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+    
+    @BeforeEach
+    void init () {}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
