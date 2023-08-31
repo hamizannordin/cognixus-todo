41e193f063521889fe969e0d08f8eed130a09ac4
hamizannordin <hamizan.nordin@gmail.com>
2023-08-31 13:30:59 +0800

    db: change ToDo id generatedvalue

--- src/main/java/cognixus/todo/entity/ToDo.java
+++ src/main/java/cognixus/todo/entity/ToDo.java
@@ -4,8 +4,9 @@ package cognixus.todo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 /**
  *
@@ -15,7 +16,15 @@ import jakarta.persistence.Id;
 public class ToDo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+      name = "sequence-generator",
+      parameters = {
+        @Parameter(name = "sequence_name", value = "to_do_seq"),
+        @Parameter(name = "initial_value", value = "1"),
+        @Parameter(name = "increment_size", value = "1")
+        }
+    )
     private Integer id;
     
     @Column(length = 50, nullable = false)
